8bf1caa8981a1d31d15460032e79244f0d41cbb7
eukalytics <eukalytics@protonmail.com>
2021-11-03 10:42:30 +0000

    config react app with tailwind and drag-drop-container

---
+++ src/App.js
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-600 text-5xl font-bold text-center mt-10">react-drag-drop-container</h1>
+    </div>
+  );
+}
